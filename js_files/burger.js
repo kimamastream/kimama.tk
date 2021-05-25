b48c67be8023972fe3435aa7fb2eab17966d8047
salamander <zahid.lmn51>
2021-05-25 19:27:14 +0600

    ad in player page(1)

--- js_files/burger.js
+++ js_files/burger.js
@@ -24,10 +24,35 @@ setInterval(function(){
 
 
 
-// back page function////////////////
+// back page function ///////////////////////////////
+// back page function
+// back page function
+// back page function////////////////////////////////
 function goBack() {
   window.history.back();
 }
 
 
+// adbd slide///////////////////////////////////////
+// adbd slide
+// adbd slide
+// adbd slide//////////////////////////////////////
+var slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
