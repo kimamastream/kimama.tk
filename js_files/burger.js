533fbfdd5e3bd4db146f736bf9cc2bb7b27bd791
salamander <farhimzakirova@gmail.com>
2021-10-07 12:43:48 +0600

    07.10.21 12:40

--- js_files/burger.js
+++ js_files/burger.js
@@ -85,3 +85,96 @@ function showSlidesmbl() {
   dotsmbl[slideIndexmbl-1].className += " activembl";
   setTimeout(showSlidesmbl, 5000); // Change image every x seconds
 }
+
+
+
+// One Page List script///////////////////////////////////
+// One Page List script///////////////////////////////////
+// One Page List script///////////////////////////////////
+
+var ASD_LIST_COUNT = $(".anime-list li").length;
+var PAGE_DATA_COUNT = 20;
+var TOTAL_PAGE_COUNT = Math.ceil(ASD_LIST_COUNT / PAGE_DATA_COUNT);
+
+
+var pageNumber = 1;
+
+$(document).ready(function () {
+    showPage(pageNumber);
+    
+    $('#lst-prev').click(function () {
+       var tempagenmbr = pageNumber - 1;
+       
+       if (tempagenmbr > 0) {
+         pageNumber = tempagenmbr;
+       }
+  
+       showPage(pageNumber);
+    });
+    
+    $('#lst-next').click(function () {
+       var tempagenmbr = pageNumber + 1;
+       
+       if (tempagenmbr <= TOTAL_PAGE_COUNT) {
+         pageNumber = tempagenmbr;
+       }
+
+       showPage(pageNumber);
+    });
+    
+    $('#lst-goto').click(function () {
+       pageNumber = +$("#lst-page").val();
+       
+       if (!pageNumber) {
+         pageNumber = 1;
+       }
+       
+       showPage(pageNumber);
+    });
+});
+
+function showPage(page) {
+    if (page > 0 && page <= TOTAL_PAGE_COUNT) {
+        var start = (PAGE_DATA_COUNT * page) - PAGE_DATA_COUNT; 
+     
+        var showListItems = $(".anime-list li").splice(start, PAGE_DATA_COUNT);
+
+        //console.log(start, showListItems);
+        
+        $(".anime-list li").hide();
+        $(showListItems).show();
+    }
+}
+
+
+// search function script////////////////////////////
+// search function script////////////////////////////
+// search function script////////////////////////////
+
+function filterFunction() {
+  let isInputAvail = false;
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("sea-myInput");
+  filter = input.value.toLowerCase();
+  if (filter.length > 0) {
+      document.getElementById("sea-myDropdown").classList.add("sea-show");
+  } else {
+      document.getElementById("sea-myDropdown").classList.remove("sea-show");
+  }
+  div = document.getElementById("sea-myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+      txtValue = a[i].innerText;
+      if (txtValue.toLowerCase().indexOf(filter) > -1) {
+          isInputAvail = true;
+          a[i].style.display = "block";
+      } else {
+          a[i].style.display = "none";
+      }
+  }
+  if (!isInputAvail) {
+      document.getElementById("sea-noMatches").classList.add('sea-show');
+  } else {
+      document.getElementById("sea-noMatches").classList.remove('sea-show');
+  }
+}
