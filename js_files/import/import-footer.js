fa13c9241aba9299aba35a6cfed3666b4ead0f26
salamander <farhimzakirova@gmail.com>
2021-10-13 11:36:03 +0600

    one header,footer is equel all page header-footer beta

---
+++ js_files/import/import-footer.js
@@ -0,0 +1,20 @@
+document.getElementById("import-footer").innerHTML =
+`
+<div class="own_footer" id="lgt6">
+<h3>@ তথ্য হোক ঊন্মুক্ত</h3>
+<div class="button"><a href="https://www.facebook.com/kimamastream" target="_blank">
+    <div class="icon">
+      <i class="fab fa-facebook-f"></i>
+    </div>
+    <span>Facebook</span></a>
+</div>
+  <div class="pages_three">
+    <ul class="flex-footer">
+      <li><a href="../../../footers/about_us.html">About</a></li>
+      <li><a href="../../../footers/FAQ's.html">FAQ's</a></li>
+      <li><a href="../../../footers/credits.html">Credits</a></li>
+    </ul>
+  </div>
+</div>
+
+`;
